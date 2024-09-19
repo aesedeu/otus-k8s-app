653344e65d5ef94b7f9bb9cc096c3900541ef1f3
Чернов Евгений Игоревич <eugene.chernov@synchro.pro>
2024-09-19 15:26:39 +0300

    fix

--- main.sh
+++ main.sh
@@ -1,3 +1,6 @@
+# ===== I часть =====
+
+
 # Запуск FastAPI
 python app.py
 
@@ -17,6 +20,45 @@ docker push aesedeu/otus_k8s_app:2.0
 
 # Запуск k8s
 minikube start --vm-driver=docker --cpus=4 --memory=4G --nodes=2
+
+# 1 - ConfigMap
+kubectl apply -f k8s/1_ConfigMap/configmap.yaml
+kubectl delete configmap welcome-configmap
+
+# 2 - Pod
+kubectl apply -f k8s/2_Pod/pod.yaml
+kubectl delete pod otus-app-v1
+
+# 3 - Service
+kubectl apply -f k8s/3_Service/service.yaml
+kubectl port-forward svc/otus-app-service -n default 80:80 # не будет балансировать нагрузку
+kubectl delete service otus-app-service
+
+# 4 - Deployment
+kubectl apply -f k8s/4_Deployment/deployment.yaml
+# Просмотр инфо о подах
+kubectl get pods --output=wide 
+
+# 5 - Ubuntu
+kubectl apply -f k8s/5_Ubuntu_pod/ubuntu.yaml
+# Проваливаемся вовнутрь пода с убунтой
+kubectl exec -it ubuntu-pod bash
+uname -a
+apt-get update
+apt-get install curl
+# Дергаем ручку у конкретного пода. Необходимо указать актуальный ip пода внутри кластера
+curl -X POST "http://10.244.1.5:80/rec" -H "Content-Type: application/json" -d '{"Age": 30, "Sex": 1, "BloodPressure": 150, "MaxHeartRate": 160}'
+# Дергаем ручки у сервиса и видим Round-Robin
+curl -X POST "http://otus-app-service:80/rec" -H "Content-Type: application/json" -d '{"Age": 30, "Sex": 1, "BloodPressure": 150, "MaxHeartRate": 160}'
+# Циклом 30 раз
+for i in {1..30};
+do curl -X POST "http://otus-app-service:80/rec" -H "Content-Type: application/json" -d '{"Age": 30, "Sex": 1, "BloodPressure": 150, "MaxHeartRate": 160}';
+done
+exit
+
+
+# ===== II часть =====
+
 
 # Устанавливаем ArgoCD
 kubectl create namespace argocd
