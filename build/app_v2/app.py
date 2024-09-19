58234dfd993a19fc46afb3b41c1c137370925980
Чернов Евгений Игоревич <eugene.chernov@synchro.pro>
2024-09-19 12:05:42 +0300

    first commit

---
+++ build/app_v2/app.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import uvicorn
+import datetime
+import pickle
+
+api_version = "0.1.0"
+app = FastAPI(
+    title="Heart risk API",
+    description="API for the first ML model",
+    version=api_version
+)
+
+@app.on_event("startup") # deprecated
+async def load_model():
+    global model
+    model = pickle.load(open("model_lr.pkl", "rb"))
+
+    global model_name
+    model_name = "LogisticRegression"
+
+class model_data(BaseModel):
+    Age: int
+    Sex: int
+    BloodPressure: int
+    MaxHeartRate: int
+
+@app.get('/')
+def read_root():
+    return {"model_verison": api_version}
+
+@app.post('/rec', tags=["prediction"])
+async def get_heart_risk(data: model_data):
+    result = model.predict([[data.Age, data.Sex, data.BloodPressure, data.MaxHeartRate]])[0]
+    if result == 0:
+        result = 'Вы вне группы сердечного риска'
+    else:
+        result = 'Вы в группе риска, обратитесь к врачу!'
+    json_response = {
+        "timestamp": datetime.datetime.now().timestamp(),
+        "model_name": model_name,
+        "model_response": result
+    }
+    return json_response
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
