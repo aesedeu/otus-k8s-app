49ee00ad9c1ae149439dc64f0ec2b0a9210a17b4
Чернов Евгений Игоревич <eugene.chernov@synchro.pro>
2024-09-19 14:50:11 +0300

    fix

--- main.sh
+++ main.sh
@@ -30,8 +30,11 @@ kubectl port-forward svc/argocd-server -n argocd 8080:80
 # Декларативное создание приложения
 kubectl apply -f application.yaml
 
+# Можно выставить порт сервиса наружу (не будет балансировать нагрузку)
+kubectl port-forward svc/otus-app-service -n default 8000:80
+
 # Можно менять версию приложения в файле values.yaml
-# Проваливаемся во-внутрь пода с убунтой
+# Проваливаемся вовнутрь пода с убунтой
 kubectl exec -it ubuntu-pod bash
 uname -a
 apt-get update
@@ -39,11 +42,11 @@ apt-get install curl
 # Дергаем ручку у конкретного пода. Необходимо указать актуальный ip пода внутри кластера
 curl -X POST "http://10.244.1.5:80/rec" -H "Content-Type: application/json" -d '{"Age": 30, "Sex": 1, "BloodPressure": 150, "MaxHeartRate": 160}'
 # Дергаем ручки у сервиса и видим Round-Robin
-curl -X POST "http://otus-app-service:80/rec" -H "Content-Type: application/json" -d '{"Age": 30, "Sex": 1, "BloodPressure": 150, "MaxHeartRate": 160}'
+curl -v -X POST "http://otus-app-service:80/rec" -H "Content-Type: application/json" -d '{"Age": 30, "Sex": 1, "BloodPressure": 150, "MaxHeartRate": 160}'
+exit
+
+# История обновлений
+# Можно откатиться через интерфейс ArgoCD
 
 # Проброс для БД
 kubectl port-forward svc/postgres-app -n default 5432:5432
-
-
-
-cd build/app_v1 && docker build --tag aesedeu/otus_k8s_app:1.0 --file Dockerfile . && cd ../../ && cd build/app_v2 && docker build --tag aesedeu/otus_k8s_app:2.0 --file Dockerfile . && cd ../../ && docker push aesedeu/otus_k8s_app:1.0 && docker push aesedeu/otus_k8s_app:2.0
