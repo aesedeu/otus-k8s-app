94bf63f89232d85eca67a242d1e76924704f32c9
Чернов Евгений Игоревич <eugene.chernov@synchro.pro>
2024-09-19 14:29:12 +0300

    fix

--- build/app_v1/app.py
+++ build/app_v1/app.py
@@ -58,4 +58,4 @@ async def get_heart_risk(data: model_data):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
--- main.sh
+++ main.sh
@@ -2,17 +2,17 @@
 python app.py
 
 # Проверка ответа
-curl -v -X POST "http://localhost:8000/rec" -H "Content-Type: application/json" -d '{"Age": 30, "Sex": 1, "BloodPressure": 150, "MaxHeartRate": 160}'  
-curl -X POST "http://localhost:8000/rec" -H "Content-Type: application/json" -d '{"Age": 30, "Sex": 1, "BloodPressure": 150, "MaxHeartRate": 160}'  
+curl -v -X POST "http://localhost:80/rec" -H "Content-Type: application/json" -d '{"Age": 30, "Sex": 1, "BloodPressure": 150, "MaxHeartRate": 160}'  
+curl -X POST "http://localhost:80/rec" -H "Content-Type: application/json" -d '{"Age": 30, "Sex": 1, "BloodPressure": 150, "MaxHeartRate": 160}'  
 
 # Сборка модели v1
 cd build/app_v1 && docker build --tag aesedeu/otus_k8s_app:1.0 --file Dockerfile . && cd ../../
-docker run --name model_v1 -p 8000:8000 -d aesedeu/otus_k8s_app:1.0
+docker run --name model_v1 -p 80:80 -d aesedeu/otus_k8s_app:1.0
 docker push aesedeu/otus_k8s_app:1.0
 
 # Сборка модели v2
 cd build/app_v2 && docker build --tag aesedeu/otus_k8s_app:2.0 --file Dockerfile . && cd ../../
-docker run --name model_v2 -p 8000:8000 -d aesedeu/otus_k8s_app:2.0
+docker run --name model_v2 -p 80:80 -d aesedeu/otus_k8s_app:2.0
 docker push aesedeu/otus_k8s_app:2.0
 
 # Запуск k8s
@@ -32,3 +32,7 @@ kubectl apply -f application.yaml
 
 # Проброс для БД
 kubectl port-forward svc/postgres-app -n default 5432:5432
+
+
+
+cd build/app_v1 && docker build --tag aesedeu/otus_k8s_app:1.0 --file Dockerfile . && cd ../../ && cd build/app_v2 && docker build --tag aesedeu/otus_k8s_app:2.0 --file Dockerfile . && cd ../../ && docker push aesedeu/otus_k8s_app:1.0 && docker push aesedeu/otus_k8s_app:2.0
